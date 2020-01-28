bf74873faa2dfd9da8d161c0ff2f31340d14c11f
danielgil1 <dfgilto@gmail.com>
2020-01-28 23:13:28 +1100

    WIP: Added SCD type 3 to DimCustomers, pending location

--- dw_overhill.sql
+++ dw_overhill.sql
@@ -4,7 +4,6 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-
 -- -----------------------------------------------------
 -- Schema dw_overhill
 -- -----------------------------------------------------
@@ -114,21 +113,15 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`DimLocations` (
 DROP TABLE IF EXISTS `dw_overhill`.`DimCustomers` ;
 
 CREATE TABLE IF NOT EXISTS `dw_overhill`.`DimCustomers` (
-  `customer_id` INT NOT NULL AUTO_INCREMENT,
-  `IndustryName` VARCHAR(50) NULL,
-  `CompanyName` VARCHAR(50) NOT NULL,
-  `Name` VARCHAR(50) NOT NULL,
-  `Phone` VARCHAR(50) NOT NULL,
-  `Email` VARCHAR(50) NOT NULL,
-  `Website` VARCHAR(50) NULL DEFAULT NULL,
-  `LastAddress` VARCHAR(50) NULL,
-  `CurrentAddress` VARCHAR(50) NOT NULL,
-  `LocationID` INT NOT NULL,
-  `isActive` INT NOT NULL,
-  PRIMARY KEY (`customer_id`, `LocationID`),
+  `CustomerID` INT NOT NULL AUTO_INCREMENT,
+  `CustKey` INT NULL,
+  `Name` VARCHAR(50) NULL,
+  `LastAddress` VARCHAR(500) NULL,
+  `CurrentAddress` VARCHAR(500) NULL,
+  `LocationID` INT NULL,
+  `LastUpdated` DATE NULL,
+  PRIMARY KEY (`CustomerID`),
   INDEX `fk_customer_dim_location1_idx` (`LocationID` ASC) VISIBLE,
-  INDEX `idx_industry_name` (`IndustryName` ASC) VISIBLE,
-  INDEX `idx_company_name` (`CompanyName` ASC) VISIBLE,
   INDEX `idx_name` (`Name` ASC) VISIBLE,
   CONSTRAINT `fk_customer_dim_location1`
     FOREIGN KEY (`LocationID`)
@@ -217,12 +210,12 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`WeeklyGrainSalesFact` (
   `TotalMargin` DECIMAL(7,2) NOT NULL,
   PRIMARY KEY (`SalesID`),
   INDEX `FK` (`ProductID` ASC, `CustomerID` ASC, `AgentID` ASC) VISIBLE,
-  INDEX `fk_weekly_grain_sales_fact_customer_dim1_idx` (`CustomerID` ASC) VISIBLE,
   INDEX `fk_weekly_grain_sales_fact_product_dim_idx` (`ProductID` ASC) VISIBLE,
   INDEX `fk_weekly_grain_sales_fact_agent_dim_idx` (`AgentID` ASC) VISIBLE,
   INDEX `idx_total_margin` (`TotalMargin` ASC) VISIBLE,
   INDEX `fk_WeeklyGrainSalesFact_DimDates1_idx` (`TimeID` ASC) VISIBLE,
   INDEX `fk_WeeklyGrainSalesFact_DimMarkets1_idx` (`MarketID` ASC) VISIBLE,
+  INDEX `fk_weekly_grain_sales_fact_customer_dim1_idx` (`CustomerID` ASC) VISIBLE,
   CONSTRAINT `fk_weekly_grain_sales_fact_product_dim1`
     FOREIGN KEY (`ProductID`)
     REFERENCES `dw_overhill`.`DimProducts` (`ProductID`)
@@ -230,7 +223,7 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`WeeklyGrainSalesFact` (
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_weekly_grain_sales_fact_customer_dim1`
     FOREIGN KEY (`CustomerID`)
-    REFERENCES `dw_overhill`.`DimCustomers` (`customer_id`)
+    REFERENCES `dw_overhill`.`DimCustomers` (`CustomerID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_WeeklyGrainSalesFact_DimAgents1`
@@ -268,12 +261,12 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`TxGrainSalesFact` (
   `CommissionAmount` DECIMAL(7,2) NOT NULL,
   PRIMARY KEY (`SalesID`),
   INDEX `FK` (`ProductID` ASC, `CustomerID` ASC, `AgentID` ASC) VISIBLE,
-  INDEX `fk_tx_grain_sales_fact_customer_dim1_idx` (`CustomerID` ASC) VISIBLE,
   INDEX `fk_tx_grain_sales_fact_product_dim_idx` (`ProductID` ASC) VISIBLE,
   INDEX `fk_tx_grain_sales_fact_agent_dim_idx` (`AgentID` ASC) VISIBLE,
   INDEX `idx_total_margin` (`Margin` ASC) VISIBLE,
   INDEX `fk_TxGrainSalesFact_DimDates1_idx` (`TimeID` ASC) VISIBLE,
   INDEX `fk_TxGrainSalesFact_DimMarkets1_idx` (`MarketID` ASC) VISIBLE,
+  INDEX `fk_weekly_grain_sales_fact_customer_dim10_idx` (`CustomerID` ASC) VISIBLE,
   CONSTRAINT `fk_weekly_grain_sales_fact_product_dim10`
     FOREIGN KEY (`ProductID`)
     REFERENCES `dw_overhill`.`DimProducts` (`ProductID`)
@@ -281,7 +274,7 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`TxGrainSalesFact` (
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_weekly_grain_sales_fact_customer_dim10`
     FOREIGN KEY (`CustomerID`)
-    REFERENCES `dw_overhill`.`DimCustomers` (`customer_id`)
+    REFERENCES `dw_overhill`.`DimCustomers` (`CustomerID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_TxGrainSalesFact_DimAgents1`
