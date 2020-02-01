f7edc8778d631273b174d317b45c31dd8fc89e23
chaitramouli <chaitramouli94@gmail.com>
2020-02-01 15:19:23 +1100

    updated agents

--- dw_overhill.sql
+++ dw_overhill.sql
@@ -1,5 +1,3 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -24,8 +22,8 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`DimHistCost` (
   `AvgProdCost` DECIMAL(7,4) NOT NULL,
   `PackageSize` INT NOT NULL,
   PRIMARY KEY (`CostID`),
-  INDEX `idx_year` (`ProdYear` ASC) VISIBLE,
-  INDEX `idx_product` (`ProdCode` ASC) VISIBLE);
+  INDEX `idx_year` (`ProdYear` ASC) ,
+  INDEX `idx_product` (`ProdCode` ASC) );
 
 
 -- -----------------------------------------------------
@@ -46,10 +44,10 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`DimProducts` (
   `PriceDateFrom` DATE NULL,
   `PriceDateTo` DATE NULL,
   PRIMARY KEY (`ProductID`),
-  INDEX `idx_group` (`Group` ASC) VISIBLE,
-  INDEX `idx_type` (`Type` ASC) VISIBLE,
-  INDEX `idx_brand` (`Brand` ASC) VISIBLE,
-  INDEX `fk_DimProducts_DimHistCost1_idx` (`CostID` ASC) VISIBLE,
+  INDEX `idx_group` (`Group` ASC) ,
+  INDEX `idx_type` (`Type` ASC) ,
+  INDEX `idx_brand` (`Brand` ASC) ,
+  INDEX `fk_DimProducts_DimHistCost1_idx` (`CostID` ASC) ,
   CONSTRAINT `fk_DimProducts_DimHistCost1`
     FOREIGN KEY (`CostID`)
     REFERENCES `dw_overhill`.`DimHistCost` (`CostID`)
@@ -66,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`DimCities` (
   `CityID` INT NOT NULL AUTO_INCREMENT,
   `CityName` VARCHAR(50) NULL,
   PRIMARY KEY (`CityID`),
-  INDEX `city_name_idx` (`CityName` ASC) VISIBLE);
+  INDEX `city_name_idx` (`CityName` ASC) );
 
 
 -- -----------------------------------------------------
@@ -80,8 +78,8 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`DimLocations` (
   `LocationName` VARCHAR(50) NOT NULL,
   `PostCode` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`LocationID`),
-  INDEX `fk_city_id_idx` (`CityID` ASC) VISIBLE,
-  INDEX `idx_postcode` (`PostCode` ASC) VISIBLE,
+  INDEX `fk_city_id_idx` (`CityID` ASC)   ,
+  INDEX `idx_postcode` (`PostCode` ASC)   ,
   CONSTRAINT `fk_DimLocations_DimCities1`
     FOREIGN KEY (`CityID`)
     REFERENCES `dw_overhill`.`DimCities` (`CityID`)
@@ -104,9 +102,9 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`DimCustomers` (
   `LastUpdated` DATE NULL,
   `Market` VARCHAR(50) NULL,
   PRIMARY KEY (`CustomerID`),
-  INDEX `fk_customer_dim_location1_idx` (`LocationID` ASC) VISIBLE,
-  INDEX `idx_name` (`Name` ASC) VISIBLE,
-  INDEX `idx_market` (`Market` ASC) VISIBLE,
+  INDEX `fk_customer_dim_location1_idx` (`LocationID` ASC)   ,
+  INDEX `idx_name` (`Name` ASC)   ,
+  INDEX `idx_market` (`Market` ASC)   ,
   CONSTRAINT `fk_customer_dim_location1`
     FOREIGN KEY (`LocationID`)
     REFERENCES `dw_overhill`.`DimLocations` (`LocationID`)
@@ -121,11 +119,12 @@ DROP TABLE IF EXISTS `dw_overhill`.`DimAgents` ;
 
 CREATE TABLE IF NOT EXISTS `dw_overhill`.`DimAgents` (
   `AgentID` INT NOT NULL AUTO_INCREMENT,
+  `AgentKey` VARCHAR(10) NOT NULL ,
   `AgentName` VARCHAR(50) NOT NULL,
   `CommissionRate` DECIMAL(2,2) NOT NULL,
   `AgentStartDate` DATETIME NOT NULL,
   `AgentFinishDate` DATETIME NOT NULL DEFAULT '9999-12-31',
-  INDEX `idx_agent_name` (`AgentName` ASC) VISIBLE,
+  INDEX `idx_agent_name` (`AgentName` ASC)   ,
   PRIMARY KEY (`AgentID`));
 
 
@@ -153,11 +152,11 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`DimDates` (
   `YearQuarterNum` INT NOT NULL,
   `DayNumOfQuarter` INT NOT NULL,
   PRIMARY KEY (`TimeID`),
-  INDEX `idx_week` (`WeekNum` ASC) VISIBLE,
-  INDEX `idx_month` (`MonthName` ASC) VISIBLE,
-  INDEX `idx_quarter` (`Calendar_Quarter` ASC) VISIBLE,
-  INDEX `idx_date` (`Date` ASC) VISIBLE,
-  INDEX `idx_day` (`DayNumOfMonth` ASC) VISIBLE);
+  INDEX `idx_week` (`WeekNum` ASC) ,
+  INDEX `idx_month` (`MonthName` ASC) ,
+  INDEX `idx_quarter` (`Calendar_Quarter` ASC)   ,
+  INDEX `idx_date` (`Date` ASC)   ,
+  INDEX `idx_day` (`DayNumOfMonth` ASC)   );
 
 
 -- -----------------------------------------------------
@@ -196,13 +195,13 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`WeeklyGrainSalesFact` (
   `TotalCost` DECIMAL(7,2) NOT NULL,
   `TotalMargin` DECIMAL(7,2) NOT NULL,
   PRIMARY KEY (`SalesID`),
-  INDEX `FK` (`ProductID` ASC, `CustomerID` ASC, `AgentID` ASC) VISIBLE,
-  INDEX `fk_weekly_grain_sales_fact_product_dim_idx` (`ProductID` ASC) VISIBLE,
-  INDEX `fk_weekly_grain_sales_fact_agent_dim_idx` (`AgentID` ASC) VISIBLE,
-  INDEX `idx_total_margin` (`TotalMargin` ASC) VISIBLE,
-  INDEX `fk_WeeklyGrainSalesFact_DimDates1_idx` (`TimeID` ASC) VISIBLE,
-  INDEX `fk_WeeklyGrainSalesFact_DimMarkets1_idx` (`MarketID` ASC) VISIBLE,
-  INDEX `fk_weekly_grain_sales_fact_customer_dim1_idx` (`CustomerID` ASC) VISIBLE,
+  INDEX `FK` (`ProductID` ASC, `CustomerID` ASC, `AgentID` ASC)   ,
+  INDEX `fk_weekly_grain_sales_fact_product_dim_idx` (`ProductID` ASC)   ,
+  INDEX `fk_weekly_grain_sales_fact_agent_dim_idx` (`AgentID` ASC)   ,
+  INDEX `idx_total_margin` (`TotalMargin` ASC)   ,
+  INDEX `fk_WeeklyGrainSalesFact_DimDates1_idx` (`TimeID` ASC)   ,
+  INDEX `fk_WeeklyGrainSalesFact_DimMarkets1_idx` (`MarketID` ASC)   ,
+  INDEX `fk_weekly_grain_sales_fact_customer_dim1_idx` (`CustomerID` ASC)   ,
   CONSTRAINT `fk_weekly_grain_sales_fact_product_dim1`
     FOREIGN KEY (`ProductID`)
     REFERENCES `dw_overhill`.`DimProducts` (`ProductID`)
@@ -247,13 +246,13 @@ CREATE TABLE IF NOT EXISTS `dw_overhill`.`TxGrainSalesFact` (
   `Margin` DECIMAL(7,2) NOT NULL,
   `CommissionAmount` DECIMAL(7,2) NOT NULL,
   PRIMARY KEY (`SalesID`),
-  INDEX `FK` (`ProductID` ASC, `CustomerID` ASC, `AgentID` ASC) VISIBLE,
-  INDEX `fk_tx_grain_sales_fact_product_dim_idx` (`ProductID` ASC) VISIBLE,
-  INDEX `fk_tx_grain_sales_fact_agent_dim_idx` (`AgentID` ASC) VISIBLE,
-  INDEX `idx_total_margin` (`Margin` ASC) VISIBLE,
-  INDEX `fk_TxGrainSalesFact_DimDates1_idx` (`TimeID` ASC) VISIBLE,
-  INDEX `fk_TxGrainSalesFact_DimMarkets1_idx` (`MarketID` ASC) VISIBLE,
-  INDEX `fk_weekly_grain_sales_fact_customer_dim10_idx` (`CustomerID` ASC) VISIBLE,
+  INDEX `FK` (`ProductID` ASC, `CustomerID` ASC, `AgentID` ASC)   ,
+  INDEX `fk_tx_grain_sales_fact_product_dim_idx` (`ProductID` ASC)   ,
+  INDEX `fk_tx_grain_sales_fact_agent_dim_idx` (`AgentID` ASC)   ,
+  INDEX `idx_total_margin` (`Margin` ASC)   ,
+  INDEX `fk_TxGrainSalesFact_DimDates1_idx` (`TimeID` ASC)   ,
+  INDEX `fk_TxGrainSalesFact_DimMarkets1_idx` (`MarketID` ASC)   ,
+  INDEX `fk_weekly_grain_sales_fact_customer_dim10_idx` (`CustomerID` ASC)   ,
   CONSTRAINT `fk_weekly_grain_sales_fact_product_dim10`
     FOREIGN KEY (`ProductID`)
     REFERENCES `dw_overhill`.`DimProducts` (`ProductID`)
